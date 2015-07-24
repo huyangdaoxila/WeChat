f7ed2db76306aa11324aede703d23380fca59e88
huyang <daoxila@iMac.local>
2015-07-24 16:40:06 +0800

    Initial Commit

---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by huyang on 15/7/24.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeChat
+//
+//  Created by huyang on 15/7/24.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
